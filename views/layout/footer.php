5d943ed694b63a35e194f6f210e28c49707d7049
sofiaazhra <151343513+sofiaazhra@users.noreply.github.com>
2025-06-13 11:10:50 +0700

    Create footer.php

---
+++ views/layout/footer.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+        <div class="footer-content">
+            <p>&copy; 2025 CeritaRasa. All rights reserved.</p>
+        </div>
+    </footer>
+
+    <script src="/js/script.js"></script>
+</body>
+</html>
